eb76c5a9d620302aa3de43ab6bc086d497833517
Javier Infante <jabi@irontec.com>
2019-10-04 15:16:35 +0200

    Updates

--- precache-manifest.267ac754e0737b97ba82c5e3b5aa8f46.js
+++ precache-manifest.267ac754e0737b97ba82c5e3b5aa8f46.js
@@ -1,15 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "e234f91e664153332d70",
+    "revision": "d39a3501cbab0428dd8b",
     "url": "app.css"
   },
   {
-    "revision": "e234f91e664153332d70",
+    "revision": "d39a3501cbab0428dd8b",
     "url": "app.js"
   },
   {
     "revision": "6fbfc5d6ff3d3b166f12",
     "url": "hljs-langs/bash.js"
+  },
+  {
+    "revision": "bf45ef848f036d3234652ac6795328e7",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
   },
   {
     "revision": "9801ff92c1d4e5778d71878ab20bb7a8",
@@ -18,10 +22,6 @@ self.__precacheManifest = [
   {
     "revision": "172133917c3c23a27512b50927e278b9",
     "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
-  },
-  {
-    "revision": "bf45ef848f036d3234652ac6795328e7",
-    "url": "b97c530867a61ea60506b10a416762c4.svg"
   },
   {
     "revision": "5d039d9f3408836ef24841905f04b69b",
@@ -48,22 +48,22 @@ self.__precacheManifest = [
     "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
   },
   {
-    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
   },
   {
     "url": "0fc25386220a58203994ce45fb4ae570.ttf"
   },
   {
-    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
   },
   {
-    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
   },
   {
     "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
   },
   {
-    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
   },
   {
     "url": "0ac32fc069cbe0042741ae8c29d8e426.jpg"
@@ -85,15 +85,15 @@ self.__precacheManifest = [
     "url": "media/firefox_socks5.png"
   },
   {
-    "revision": "d3fd5607f64884ec1cac83728ea47bb8",
-    "url": "media/ssh_forwarding.png"
-  },
-  {
     "revision": "801c5b55287ce655d4ba26329fa2cfa5",
     "url": "media/proxy_jump.png"
   },
   {
-    "revision": "98ca55dbe7009167be387a2ba5cf5822",
+    "revision": "d3fd5607f64884ec1cac83728ea47bb8",
+    "url": "media/ssh_forwarding.png"
+  },
+  {
+    "revision": "2d2074e8152c30ba0c1652778a9b1393",
     "url": "index.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d891b6430f5fa4677806c79cd9db445c.js"
+  "precache-manifest.267ac754e0737b97ba82c5e3b5aa8f46.js"
 );
 
 workbox.skipWaiting();
