059fa19b65c104a574ef90f34d86883521284ef1
Javier Infante <jabi@irontec.com>
2019-10-04 13:35:10 +0200

    Updates

---
+++ precache-manifest.1c64d7639556f5e6ef27859000558441.js
@@ -0,0 +1,103 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a27bdfc495e0e54a43f",
+    "url": "app.css"
+  },
+  {
+    "revision": "0a27bdfc495e0e54a43f",
+    "url": "app.js"
+  },
+  {
+    "revision": "6fbfc5d6ff3d3b166f12",
+    "url": "hljs-langs/bash.js"
+  },
+  {
+    "revision": "bf45ef848f036d3234652ac6795328e7",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
+  },
+  {
+    "revision": "9801ff92c1d4e5778d71878ab20bb7a8",
+    "url": "95c7b5a26ab51d96ee39f36c66005d67.svg"
+  },
+  {
+    "revision": "172133917c3c23a27512b50927e278b9",
+    "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
+  },
+  {
+    "revision": "5d039d9f3408836ef24841905f04b69b",
+    "url": "1ba934df20c60bfbda92ae00e4ac1b37.svg"
+  },
+  {
+    "revision": "43b843880f019c3bdf2a047b201961a7",
+    "url": "187a99d3fe4e62c75737c5f4a93e00a6.svg"
+  },
+  {
+    "revision": "e688707ed86fca73e90fd1e2d3dc4645",
+    "url": "a0296bb86117cab30ef27fd515a87f79.svg"
+  },
+  {
+    "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
+  },
+  {
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+  },
+  {
+    "url": "fc84e998bc29b297ea20321e4c90b6ed.ttf"
+  },
+  {
+    "url": "3e1af3ef546b9e6ecef9f3ba197bf7d2.ttf"
+  },
+  {
+    "url": "89e2666c24d37055bcb60e9d2d9f7e35.ttf"
+  },
+  {
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+  },
+  {
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+  },
+  {
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+  },
+  {
+    "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
+  },
+  {
+    "url": "0fc25386220a58203994ce45fb4ae570.ttf"
+  },
+  {
+    "url": "0ac32fc069cbe0042741ae8c29d8e426.jpg"
+  },
+  {
+    "revision": "2bcc33156db7890cc967718376c1ffd1",
+    "url": "3rd.party.LICENSES"
+  },
+  {
+    "revision": "0ac32fc069cbe0042741ae8c29d8e426",
+    "url": "media/home_background.jpg"
+  },
+  {
+    "revision": "9a1ae21b4d68eed1bfa698ffbd837a62",
+    "url": "media/firefox_socks5.png"
+  },
+  {
+    "revision": "d3fd5607f64884ec1cac83728ea47bb8",
+    "url": "media/ssh_forwarding.png"
+  },
+  {
+    "revision": "801c5b55287ce655d4ba26329fa2cfa5",
+    "url": "media/proxy_jump.png"
+  },
+  {
+    "revision": "c5e74152a707a5ba2ef642c9aca2b904",
+    "url": "media/logo_irontec.png"
+  },
+  {
+    "revision": "17d3b35923f85b6b0f60f08ce95771ec",
+    "url": "index.html"
+  },
+  {
+    "revision": "27ddf47c7a7de1b45858d6d59a4017f5",
+    "url": "notes.html"
+  }
+];
